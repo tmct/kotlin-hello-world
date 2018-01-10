1b1e7f601643de83c1222c891979402ec38896a4
Tom McTiernan <thomasmctiernan@gmail.com>
2018-01-10 22:57:14 +0000

    Get ready for proj euler solutions

---
+++ src/test/kotlin/euler/problem57/tests.kt
@@ -0,0 +1,10 @@
+package euler.problem57
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class TestSource {
+    @Test fun f() {
+        assertEquals("Hello, world!", getGreeting())
+    }
+}
